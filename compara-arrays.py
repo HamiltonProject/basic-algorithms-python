26560e9abd008e86d2dca9aa893893f3f1f9376c
Marco Paulo Ollivier <marcopollivier@gmail.com>
2018-09-18 02:40:36 -0300

    Altera informacoes do array

--- compara-arrays.py
+++ compara-arrays.py
@@ -1,9 +1,9 @@
 # Retornar interseção entre os dois arrays
 
-array_base = [0,1, 1, 2, 3, 5, 8, 13, 21]
-array_comparacao = [1, 2, 3]
+array_base = [0, 1, 1, 2, 3, 5, 8, 13, 21, 21, 21, 9, 1]
+array_comparacao = [1, 2, 3, 9, 10, 100, 2, 11, 1, 9, 2, 3]
 
-#Retornar 1, 2 e 3
+#Retornar 1, 2, 3 e 9
 
 for indice_base in array_base:
     for indice_comparacao in array_comparacao:
