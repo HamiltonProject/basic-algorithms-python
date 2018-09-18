83827fcef4e729a2938b171fc8a80abb2240511b
Marco Paulo Ollivier <marcopollivier@gmail.com>
2018-09-18 02:55:37 -0300

    Corrige padroes do codigo

--- compara-arrays.py
+++ compara-arrays.py
@@ -3,7 +3,7 @@
 array_base = [0, 1, 1, 2, 3, 5, 8, 13, 21, 21, 21, 9, 1]
 array_comparacao = [1, 2, 3, 9, 10, 100, 2, 11, 1, 9, 2, 3]
 
-#Retornar 1, 2, 3 e 9
+# Retornar 1, 2, 3 e 9
 
 for indice_base in array_base:
     for indice_comparacao in array_comparacao:
