8eb82dbb5550863d03127635bd0c4238353befa6
Marco Paulo Ollivier <marcopollivier@users.noreply.github.com>
2020-03-21 16:45:48 -0300

    Primeira solucao - forca bruta

--- compara-arrays.py
+++ compara-arrays.py
@@ -5,6 +5,7 @@ array_comparacao = [1, 2, 3, 9, 10, 100, 2, 11, 1, 9, 2, 3]
 
 # Retornar 1, 2, 3 e 9
 
+# forca bruta
 for indice_base in array_base:
     for indice_comparacao in array_comparacao:
         if indice_base == indice_comparacao:
